beef8830c3142ec0c2d6ece6f58e7a2c91ca79b9
David Ron <dron@cj.com>
2016-02-04 14:31:12 -0800

    2016-02-04 Winston Had Fun

--- game.py
+++ game.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 
-class MyClass:
-    def f(self):
-        return 'hello world'
+class Game:
+    doorOpen=False
+    def message(self, command):
+        #Here is where we are dealing with what they typed and channging state
+        if("open door" in command):
+            self.doorOpen=True            
+       
+
+        #Here is where we show a message based on the state.
+        if (self.doorOpen == True):
+            if ("look around" in command):
+                return "You look around and notice, the room is dark.."
+            if ("look down" in command):
+                return "You see a box of matches on your belt."   
+
+            return "The Door Is Open"
+        
+        return 'You are at a door.'
+        
+    def prompt(self):
+        command = raw_input("> ")
+        print self.message(command)
 
 
-"""Create a new MyClass object, call f() on it, and then print the output """
-print MyClass().f()
+game = Game()
+print "Welcome!"
+while True :
+    game.prompt()
