2a4dded6167dad3648c694371b7aa06058e4cdf5
David Ron <dron@cj.com>
2016-02-04 12:31:45 -0800

    Some initial python class in a python file.

---
+++ game.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+class MyClass:
+    def f(self):
+        return 'hello world'
+
+
+"""Create a new MyClass object, call f() on it, and then print the output """
+print MyClass().f()
