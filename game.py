f9f43c5a918e0022502219bf6536f5fdf425c375
David Ron <dron@cj.com>
2016-03-03 14:33:08 -0800

    2016-03-03

--- game.py
+++ game.py
@@ -7,21 +7,29 @@ class Outside:
         return Response(self, "You are at a door.")  
 
 class DarkRoom:
-    lookBehindPrompt = False;
+    haveWeLookedAround=False
+    matchLit=False    
     def message(self, command):
+        if (self.matchLit):
+            if(self.haveWeLookedAround):
+                if("yes" in command):
+                    return Response(BaseRoom(),"The room is light and there are people,")
+            if ("yes" in command):
+                self.haveWeLookedAround=True            
+                return Response(self, "Behind you and behind the open door there is another door. Under the crack of the door you can see light, enter the door? (Yes/No)")
+            return Response(self, "The room is light and there are people,")
         if ("look around" in command):
             return Response(self, "You look around and notice, the room is dark..")
         if ("look down" in command):
             return Response(self, "You see a box of matches on your belt.")
         if ("light match" in command):
-            self.lookBehindPrompt=True
-            return Response(self, "The match lights up for 5 seconds, in that time a sign ahead of you reads \"look behind you\" \n Look behind you (Yes/No)?")
-        if(self.lookBehindPrompt):
-            if("yes" in command):  ####THERE IS MUCH TO DO HERE
-                return Response(self, "Behind you and behind the open door there is another door. Under the crack of the door you can see light.")
+            self.matchLit=True
+            return Response(self, "The match lights up for 5 seconds, in that time a sign ahead of you reads \"look behind you\" \n Look behind you? (Yes/No)")
         return Response(self, "The Door Is Open")
 
-   
+class BaseRoom:
+    def message(self, command):
+        return Response(self, "You have reached the base.")
 
 class Response:
     room = Outside()    
