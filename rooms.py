12ac184ed874087e56a6fc5d41f6a85897298cd6
David Ron <dron@cj.com>
2016-04-28 14:26:59 -0700

    2016-04-28

--- rooms.py
+++ rooms.py
@@ -1,13 +1,34 @@
+class Hahaverycleverroomname:
+    def message(self, command):
+        return Response(self, "???: Hi there!")
+
+
+
+
+
+
+
+
 class BaseRoom:
     havewelookedaround = False
     dialogue1 = False
     Yes_or_no_masked = False
     Insertgenerictrueorfalsehere = False
+    hahamorecleverstuff = False
+    yaymorecoding = False
     def message(self, command):
 
+        if (self.yaymorecoding == True):
+            return Response(Hahaverycleverroomname(), "The masked pushed you away (Turns out he isn't very nice ;(  )")
+        if (self.hahamorecleverstuff == True):
+            return Response(self, 'Masked Man: Alright take a left a right, go forward, turn back, then take 2 lefts, and a right then go 1 mile forward.. And you should be able to find the shop, ok good luck!')
+            self.yaymorecoding = True
         if (self.Insertgenerictrueorfalsehere == True):
+            if("yes" in command):
+                return Response(Hahaverycleverroomname(), "Tutorial Guy (Your only chance to figure out how to play this game): Ok then, bye..\n I'll just go, also I'm not just a masked man I'm a generic tutorial person")
             if("no" in command):
                 return Response(self, 'Ok, here is an explanation.. typing look (Up or down) will look in that direction the, most common command you will use is "look around" (Use look around too see sorroundings from left to right) ...   Next open (Insert any object here) will open things, sorry for you getting a tutorial now.. It took probably 30 minutes to figure out how to open a door, now finally try typing "inventory" to see what you have on you (type use, info, or remove to interact with your items) ......... Now finally do you want me to take you to the market? They have different stores for potions, all of the generic RPG items you can buy! ')
+                self.hahamorecleverstuff = True
         if (self.Yes_or_no_masked == True):
             self.Insertgenerictrueorfalsehere = True
             return Response(self, 'Masked Man: Do you know what you\'re doing? (Yes/No)')
@@ -21,18 +42,18 @@ class BaseRoom:
             self.dialogue1 = True
             return Response(self, "Masked Man: Are you lost?")
         return Response(self, "You have reached the base.")
-        
+
 
 
 class Outside:
     def message(self, command):
         if("open door" in command):
-            return Response(DarkRoom(), "You open the door.")    
-        return Response(self, "You are at a door.")  
+            return Response(DarkRoom(), "You open the door.")
+        return Response(self, "You are at a door.")
 
 class DarkRoom:
     haveWeLookedAround=False
-    matchLit=False    
+    matchLit=False
     def message(self, command):
         if (self.matchLit):
             if(self.haveWeLookedAround):
@@ -40,7 +61,7 @@ class DarkRoom:
                     return Response(BaseRoom(),"The room is light and there are people,")
                 return Response(self, "Under the crack of the door you can see light, enter the door? (Yes/No)")
             if ("yes" in command):
-                self.haveWeLookedAround=True            
+                self.haveWeLookedAround=True
                 return Response(self, "Behind you and behind the open door there is another door. Under the crack of the door you can see light, enter the door? (Yes/No)")
             return Response(self, 'A sign ahead of you reads "look behind you" \n Look behind you? (Yes/No)')
         if ("look around" in command):
@@ -53,11 +74,11 @@ class DarkRoom:
         return Response(self, "The door is open.")
 
 class Response:
-    room = Outside()    
+    room = Outside()
     text = "This text should never show up."
     def __init__(self, room, text):
         self.room = room
-        self.text = text    
+        self.text = text
     def getRoom(self):
         return self.room
     def getText(self):
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestStringMethods(unittest.TestCase):
   def test_isupper(self):
       self.assertTrue('FOO'.isupper())
       self.assertFalse('Foo'.isupper())
-  
+
   def testDarkRoomHappyPath(self):
       response = DarkRoom().message("")
       self.assertEqual(response.getText(), 'The door is open.')
@@ -18,16 +18,16 @@ class TestStringMethods(unittest.TestCase):
       self.assertEqual(response.getText(), 'You look around and notice, the room is dark..')
 
       response = response.getRoom().message("yes")
-      self.assertEqual(response.getText(), 'The door is open')
+      self.assertEqual(response.getText(), 'The door is open.')
 
       response = response.getRoom().message("look down")
       self.assertEqual(response.getText(), 'You see a box of matches on the stone floor, you picked up the matches (Hint: "light _____")')
-      
+
       response = response.getRoom().message("light match")
       self.assertEqual(response.getText(), 'The match lights up for 5 seconds, in that time a sign ahead of you reads "look behind you" \n Look behind you? (Yes/No)')
-      
+
       response = response.getRoom().message("look around")
-      self.assertEqual(response.getText(), 'A sign ahead of you reads "look behind you" \n Look behind you? (Yes/No)') 
+      self.assertEqual(response.getText(), 'A sign ahead of you reads "look behind you" \n Look behind you? (Yes/No)')
 
       response = response.getRoom().message("yes")
       self.assertEqual(response.getText(), 'Behind you and behind the open door there is another door. Under the crack of the door you can see light, enter the door? (Yes/No)')
@@ -48,7 +48,7 @@ class TestStringMethods(unittest.TestCase):
 
       response = response.getRoom().message("look around")
       self.assertEqual(response.getText(), 'There is a tall skinny man with a mask on.')
-      
+
       response = response.getRoom().message("fdkdfwsj")
       self.assertEqual(response.getText(), 'Masked Man: Are you lost?')
 
@@ -59,5 +59,29 @@ class TestStringMethods(unittest.TestCase):
       self.assertEqual(response.getText(), 'Masked Man: Do you know what you\'re doing? (Yes/No)')
 
       response = response.getRoom().message("no")
-      self.assertEqual(response.getText(), 'Ok, here is an explanation.. typing look (Up or down) will look in that direction the, most common command you will use is "look around" \n(Use look around too see sorroundings from left to right) ... Next open (Insert any object here) will open things, sorry for you getting a tutorial now.. It took probably 30 minutes to figure out how to open a door, now finally try typing "inventory" to see what you have on you \n(type use, info, or remove to interact with your items) ......... Now finally do you want me to take you to the market? They have different stores for potions, all of the generic RPG items you can buy! ')
+      self.assertEqual(response.getText(), 'Ok, here is an explanation.. typing look (Up or down) will look in that direction the, most common command you will use is "look around" (Use look around too see sorroundings from left to right) ...   Next open (Insert any object here) will open things, sorry for you getting a tutorial now.. It took probably 30 minutes to figure out how to open a door, now finally try typing "inventory" to see what you have on you (type use, info, or remove to interact with your items) ......... Now finally do you want me to take you to the market? They have different stores for potions, all of the generic RPG items you can buy! ')
 
+
+  def testBaseRoomNoHelp(self):
+      response = BaseRoom().message("")
+      self.assertEqual(response.getText(), 'You have reached the base.')
+
+      response = response.getRoom().message("look around")
+      self.assertEqual(response.getText(), 'There is a tall skinny man with a mask on.')
+
+      response = response.getRoom().message("fdkdfwsj")
+      self.assertEqual(response.getText(), 'Masked Man: Are you lost?')
+
+      response = response.getRoom().message("dfuskjfhadsy")
+      self.assertEqual(response.getText(), 'Masked Man: Would you like some help?')
+
+      response = response.getRoom().message("fdkdfwsj")
+      self.assertEqual(response.getText(), 'Masked Man: Do you know what you\'re doing? (Yes/No)')
+
+      response = response.getRoom().message("yes")
+      self.assertEqual(response.getText(), "Tutorial Guy (Your only chance to figure out how to play this game): Ok then, bye..\n I'll just go, also I'm not just a masked man I'm a generic tutorial person")
+      self.assertTrue(isinstance(response.getRoom(), Hahaverycleverroomname))
+
+  def testhahaverycleverroomnameHappyPath(self):
+    response = Hahaverycleverroomname().message('')
+    self.assertEqual(response.getText(), "???: Hi there!")
